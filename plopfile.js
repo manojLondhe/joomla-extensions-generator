2aa6bd6c4afccc8d76f5411c6c0da17774926c4b
Manoj L <manoj_l@techjoomla.com>
2019-11-26 14:51:20 +0530

    Feat: Add changes for plugin, module, component

--- plopfile.js
+++ plopfile.js
@@ -51,7 +51,7 @@ module.exports = function (plop) {
 				type: 'input',
 				name: 'author',
 				message: 'What is your plugin author name?',
-				default: 'Techjomla',
+				default: 'Techjoomla',
 				validate: function (value) {
 					if ((/.+/).test(value)) {
 						return true;
@@ -73,7 +73,7 @@ module.exports = function (plop) {
 				type: 'input',
 				name: 'authorUrl',
 				message: 'What is your plugin author Url?',
-				default: 'https://techjoomla.com',
+				default: 'https:/techjoomla.com',
 				validate: function (value) {
 					if ((/.+/).test(value)) {
 						return true;
@@ -84,7 +84,7 @@ module.exports = function (plop) {
 				type: 'input',
 				name: 'copyright',
 				message: 'What is your copyright?',
-				default: 'Copyright (C) 2009 - 2018 Techjoomla. All rights reserved.',
+				default: 'Copyright (C) 2009 - 2019 Techjoomla. All rights reserved.',
 				validate: function (value) {
 					if ((/.+/).test(value)) {
 						return true;
@@ -95,7 +95,7 @@ module.exports = function (plop) {
 				type: 'input',
 				name: 'licence',
 				message: 'What is your plugin licence?',
-				default: 'http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL',
+				default: 'http:/www.gnu.org/licenses/gpl-2.0.html GNU/GPL',
 				validate: function (value) {
 					if ((/.+/).test(value)) {
 						return true;
@@ -116,6 +116,12 @@ module.exports = function (plop) {
 					type: 'add',
 					path: 'output/plugins/{{ lowerCase pluginName }}/{{ lowerCase pluginName }}.xml',
 					templateFile: 'templates/plugin/plugin.xml',
+					abortOnFail: true
+				},
+				{
+					type: 'add',
+					path: 'output/plugins/{{ lowerCase pluginName }}/index.html',
+					templateFile: 'templates/plugin/index.html',
 					abortOnFail: true
 				},
 				{
@@ -175,7 +181,7 @@ module.exports = function (plop) {
 				type: 'input',
 				name: 'author',
 				message: 'What is your module author name?',
-				default: 'Techjomla',
+				default: 'Techjoomla',
 				validate: function (value) {
 					if ((/.+/).test(value)) {
 						return true;
@@ -197,7 +203,7 @@ module.exports = function (plop) {
 				type: 'input',
 				name: 'authorUrl',
 				message: 'What is your module author Url?',
-				default: 'https://techjoomla.com',
+				default: 'https:/techjoomla.com',
 				validate: function (value) {
 					if ((/.+/).test(value)) {
 						return true;
@@ -208,7 +214,7 @@ module.exports = function (plop) {
 				type: 'input',
 				name: 'copyright',
 				message: 'What is your copyright?',
-				default: 'Copyright (C) 2009 - 2018 Techjoomla. All rights reserved.',
+				default: 'Copyright (C) 2009 - 2019 Techjoomla. All rights reserved.',
 				validate: function (value) {
 					if ((/.+/).test(value)) {
 						return true;
@@ -218,8 +224,8 @@ module.exports = function (plop) {
 			{
 				type: 'input',
 				name: 'licence',
-				message: 'What is your plugin licence?',
-				default: 'http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL',
+				message: 'What is your module licence?',
+				default: 'http:/www.gnu.org/licenses/gpl-2.0.html GNU/GPL',
 				validate: function (value) {
 					if ((/.+/).test(value)) {
 						return true;
@@ -250,8 +256,20 @@ module.exports = function (plop) {
 				},
 				{
 					type: 'add',
+					path: 'output/modules/{{ lowerCase moduleName }}/index.html',
+					templateFile: 'templates/module/index.html',
+					abortOnFail: true
+				},
+				{
+					type: 'add',
 					path: 'output/modules/{{ lowerCase moduleName }}/tmpl/default.php',
 					templateFile: 'templates/module/tmpl/default.php',
+					abortOnFail: true
+				},
+				{
+					type: 'add',
+					path: 'output/modules/{{ lowerCase moduleName }}/tmpl/index.html',
+					templateFile: 'templates/module/tmpl/index.html',
 					abortOnFail: true
 				},
 				{
@@ -267,6 +285,197 @@ module.exports = function (plop) {
 					abortOnFail: true
 				}
 			];
+
+			return actions;
+		}
+	});
+
+	// Component generator
+	plop.setGenerator('Create MVC skeleton for a view', {
+		description: 'Lets you create MVC skeleton for view - model, controller, view',
+		prompts: [
+			{
+				type: 'input',
+				name: 'componentName',
+				message: 'What is your component name (skipping `com_` part eg: users)?',
+				validate: function (value) {
+					if ((/.+/).test(value)) { return true; }
+					return 'component name is required';
+				}
+			},
+			{
+				type: 'list',
+				name: 'viewType',
+				message: 'What type of view you want to build?',
+				choices: ['list', 'form', 'details'],
+				validate: function (value) {
+					if ((/.+/).test(value)) { return true; }
+					return 'view type is required';
+				}
+			},
+			{
+				type: 'input',
+				name: 'viewName',
+				message: 'What is your view name (eg: records( list) / recordform (form)/ record (details))?',
+				validate: function (value) {
+					if ((/.+/).test(value)) { return true; }
+					return 'view name is required';
+				}
+			},
+			{
+				type: 'input',
+				name: 'entityName',
+				message: 'What is your entity name (eg: student / record / product)?',
+				validate: function (value) {
+					if ((/.+/).test(value)) { return true; }
+					return 'entity name is required';
+				}
+			},
+			{
+				type: 'input',
+				name: 'tableName',
+				message: 'What is your table name without table prefix (eg: table_name)?',
+				validate: function (value) {
+					if ((/.+/).test(value)) { return true; }
+					return 'table name is required';
+				}
+			},
+			{
+				type: 'input',
+				name: 'sortingColumn',
+				message: 'What is default sorting column?',
+				default: 'id',
+				validate: function (value) {
+					if ((/.+/).test(value)) { return true; }
+					return 'sorting column is required';
+				}
+			},
+			{
+				type: 'input',
+				name: 'sortingDirection',
+				message: 'What is default sorting direction?',
+				default: 'desc',
+				validate: function (value) {
+					if ((/.+/).test(value)) { return true; }
+					return 'sorting direction is required';
+				}
+			},
+			{
+				type: 'input',
+				name: 'version',
+				message: 'What is your component version?',
+				default: '__DEPLOY_VERSION__',
+				validate: function (value) {
+					if ((/.+/).test(value)) {
+						return true;
+					}
+				}
+			},
+			{
+				type: 'input',
+				name: 'copyright',
+				message: 'What is your copyright?',
+				default: 'Copyright (C) 2009 - 2019 Techjoomla. All rights reserved.',
+				validate: function (value) {
+					if ((/.+/).test(value)) {
+						return true;
+					}
+				}
+			},
+			{
+				type: 'input',
+				name: 'licence',
+				message: 'What is your module licence?',
+				default: 'http:/www.gnu.org/licenses/gpl-2.0.html GNU/GPL',
+				validate: function (value) {
+					if ((/.+/).test(value)) {
+						return true;
+					}
+				}
+			},
+		],
+
+		actions: function(data) {
+			var actions = [];
+			var action = {};
+
+			switch (data.viewType)
+			{
+				case 'list':
+					action = {
+						type: 'add',
+						path: 'output/components/{{ lowerCase componentName }}/components/{{ lowerCase componentName }}/models/{{ lowerCase viewName }}.php',
+						templateFile: 'templates/component/site/models/list.php',
+						abortOnFail: true
+					};
+
+					actions.push(action);
+				break;
+
+				case 'form':
+					// Add model
+					action = {
+						type: 'add',
+						path: 'output/components/{{ lowerCase componentName }}/components/com_{{ lowerCase componentName }}/models/{{ lowerCase viewName }}.php',
+						templateFile: 'templates/component/site/models/form.php',
+						abortOnFail: true
+					};
+
+					actions.push(action);
+
+					// Add form.xml
+					action = {
+						type: 'add',
+						path: 'output/components/{{ lowerCase componentName }}/components/com_{{ lowerCase componentName }}/models/forms/{{ lowerCase viewName }}.xml',
+						templateFile: 'templates/component/site/models/forms/form.xml',
+						abortOnFail: true
+					};
+
+					actions.push(action);
+
+					// View
+					action = {
+						type: 'add',
+						path: 'output/components/{{ lowerCase componentName }}/components/com_{{ lowerCase componentName }}/views/{{ lowerCase viewName }}/view.html.php',
+						templateFile: 'templates/component/site/views/form/view.html.php',
+						abortOnFail: true
+					};
+
+					actions.push(action);
+
+					// View - tmpl
+					action = {
+						type: 'add',
+						path: 'output/components/{{ lowerCase componentName }}/components/com_{{ lowerCase componentName }}/views/{{ lowerCase viewName }}/tmpl/default.php',
+						templateFile: 'templates/component/site/views/form/tmpl/default.php',
+						abortOnFail: true
+					};
+
+					actions.push(action);
+
+					// Add controller
+					action = {
+						type: 'add',
+						path: 'output/components/{{ lowerCase componentName }}/components/com_{{ lowerCase componentName }}/controllers/{{ lowerCase viewName }}.php',
+						templateFile: 'templates/component/site/controllers/form.php',
+						abortOnFail: true
+					};
+
+					actions.push(action);
+
+					// Lang. file for form
+					action = {
+						type: 'add',
+						path: 'output/components/{{ lowerCase componentName }}/languages/en-GB/en-GB.{{ lowerCase viewName }}.ini',
+						templateFile: 'templates/component/languages/site/en-GB/en-GB.form.ini',
+						abortOnFail: true
+					};
+
+					actions.push(action);
+				break;
+
+				default:
+			}
 
 			return actions;
 		}
